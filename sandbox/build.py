cee05b9f2872f6d01339774dd738c2f569b6c694
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2019-06-07 18:52:14 +0200

    mise à jour build.py (sandbox)

--- sandbox/build.py
+++ sandbox/build.py
@@ -176,8 +176,8 @@ if images.exists():
             pdflatex(tex_file)
             pdf_file = tex_file.with_suffix('.pdf')
             pdf_file.rename(tex_file.with_suffix(tex_file.suffix + ".pdf"))
-        for file in list(l.glob("*.dvi")) + list(l.glob("*.aux")) + list(l.glob("*.log")) + \
-            list(l.glob(".fls") + list(l.glob(".fdb_latexmk"))):
+        for file in list(l.glob("*.dvi")) + list(l.glob("*.aux")) + list(l.glob("*.log")) \
+        + list(l.glob("*.fls")) + list(l.glob("*.fdb_latexmk")):
             file.unlink()
     finally:
         os.chdir(root)
