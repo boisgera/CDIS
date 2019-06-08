32c0e9165b98c49085e1f99b988842d395f1bb82
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2019-06-08 09:41:20 +0200

    refactor build.py

--- utils/install.py
+++ utils/install.py
@@ -4,6 +4,7 @@
 import pathlib
 import shutil
 
+
 def install():
     root = pathlib.Path.cwd().parent
     dirs = [path for path in root.iterdir() if path.is_dir()]
@@ -11,6 +12,7 @@ def install():
     for dir_ in dirs:
         print(f"installing build in {dir_}")
         shutil.copy("build.py", dir_ / "build")
+
 
 if __name__ == "__main__":
     install()
