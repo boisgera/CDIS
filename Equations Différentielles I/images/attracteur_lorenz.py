8789f41f9f89c65c8fa960a4281d5a644ad87ee0
paulinebernard <pau-bernard@hotmail.fr>
2020-08-22 11:05:52 +0200

    update

--- Equations Différentielles I/images/attracteur_lorenz.py
+++ Equations Différentielles I/images/attracteur_lorenz.py
@@ -15,7 +15,7 @@ from scipy.integrate import solve_ivp
 # ------------------------------------------------------------------------------
 rc = {
     "text.usetex": True,
-    "pgf.preamble": [r"\usepackage{amsmath,amsfonts,amssymb}"], 
+    "pgf.preamble": r"\usepackage{amsmath,amsfonts,amssymb}", 
     "font.family": "serif",
     #"font.serif": [],      # use latex default serif font
     #"font.sans-serif": [], # use a specific sans-serif font
